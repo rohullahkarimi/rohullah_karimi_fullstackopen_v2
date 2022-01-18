5238e6cd5a313bb4fcdc2e53090bfe609274f2f8
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-01-18 21:05:11 +0200

    part1 b is done

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 
 const Header = (props) => {
+  //console.log(props.course);
   return (
     <h1>
      {props.course}
@@ -11,50 +12,57 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  // I changed p tag to li because react sent warning for list of p tags without key value. 
+  // Warning: Each child in a list should have a unique "key" prop
+  
+  const parts = props.parts;
+  const contentList = parts.map((part) =>
+    <li key={part.exercises}>{part.name} {part.exercises}</li> 
+  );
+
   return (
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </div>
+    <ul>
+      {contentList}
+    </ul>
   )
 }
 
 const Total = (props) => {
+  const sum = props.parts.reduce((a,v) =>  a = a + v.exercises , 0 )
   return (
     <p>
-     Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+     Number of exercises {sum}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
-    return (
-      <div>
-        <Header course={course} />
-        <Content 
-          part1={part1} exercises1={exercises1}
-          part2={part2} exercises2={exercises2}
-          part3={part3} exercises3={exercises3}
-        />
-        <Total  exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3} />
-      </div>
-    )
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
 }
 
 export default App

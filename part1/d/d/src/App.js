2e7ee3e0d9e0a064b8be0368e77a511e0d16bb43
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-01-25 18:31:35 +0200

    part1 d 1.6

---
+++ part1/d/d/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const Result = (props) => {
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral
+
+  return (
+    <div>
+      <h3>statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        Good
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+
+      <Result good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App

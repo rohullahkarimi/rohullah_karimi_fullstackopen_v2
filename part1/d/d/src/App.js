bf154852625f27b617b79df364a740204cc3015c
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-01-25 19:31:02 +0200

    part1 d 1.9

--- part1/d/d/src/App.js
+++ part1/d/d/src/App.js
@@ -8,17 +8,25 @@ const Result = (props) => {
   const average = (good - bad) / all 
   const positive = (good / all) * 100
 
-  return (
+  if(all >0){
+    return (
+      <div>
+        <h3>statistics</h3>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+      </div>
+    )
+  }else{
+    return (
     <div>
-      <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+        <p>No feedback given</p>
     </div>
-  )
+    )
+  }
 }
 const App = () => {
   const [good, setGood] = useState(0)

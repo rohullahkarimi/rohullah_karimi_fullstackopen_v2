7130470eb395215edd4bb1c362531fc1d0171d56
Rohullah Karimi <rohullahahmad99@gmail.com>
2022-03-22 17:21:40 +0200

    part3 b is completed

--- part3/a/index.js
+++ part3/a/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let persons = [
   { 
@@ -24,7 +25,21 @@ let persons = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+
+app.use(requestLogger)
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -86,6 +101,12 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
